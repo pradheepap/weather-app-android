c2724f891026155a200cb349eebc13c981ab7f80
Pradheepa <pradheepa.p@gmail.com>
2021-05-12 17:16:03 +0800

    Add Sample Activity, Service

--- app/src/main/java/sg/express/weather/MainActivity.java
+++ app/src/main/java/sg/express/weather/MainActivity.java
@@ -2,13 +2,24 @@ package sg.express.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import sg.express.weather.service.MainService;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    String msg = "Android : ";
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
     }
+
 }
---
+++ app/src/main/java/sg/express/weather/activity/SampleActivity.java
@@ -0,0 +1,68 @@
+package sg.express.weather.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import sg.express.weather.R;
+import sg.express.weather.service.MainService;
+
+public class SampleActivity extends AppCompatActivity {
+
+    String msg = "Android : ";
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+        Log.d(msg, "The onCreate() event");
+    }
+
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
+    /** Called when the activity has become visible. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    /** Called when another activity is taking focus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    /** Called when the activity is no longer visible. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /** Called just before the activity is destroyed. */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MainService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MainService.class));
+    }
+}
